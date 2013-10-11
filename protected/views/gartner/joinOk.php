b635719a366b7c0b541e5936368413d9d64ae9cd
leftarrow <cranelee@gmail.com>
2013-10-11 08:40:11 +0000

    Tony Update: Confirmation Text Update

--- protected/views/gartner/joinOk.php
+++ protected/views/gartner/joinOk.php
@@ -4,10 +4,10 @@
 	<p>It is our pleasure to confirm your attendance at Gartner's Holiday Event for Fort Myers Associates.</p>
 	<p>
   <table align="center">
-			<tr align="center"><td>The Ritz-Carlton Naples</td>
+			<tr align="center"><td>Sanibel Harbour Marriott Resort & Spa</td>
 			</tr>
-			<tr align="center"><td>280 Vanderbilt Beach Road</td></tr>
-			<tr align="center"><td>Naples,Florida 34108</td></tr>
+			<tr align="center"><td>17260 Harbour Point Dr</td></tr>
+			<tr align="center"><td>Fort Myers, FL 33908</td></tr>
 			<tr align="center"><td>Friday, December 13,2013, 6:30p.m - 10:30p.m</td></tr>
   </table>
 	</p>
@@ -15,8 +15,8 @@
 	<p>Specific directions will be provided nearer the event.</p>
 	<br />
 	<p style="color:#0065a4">Accommodation:</p>
-	<p><b>The Ritz-Carlton, Naples</b><br/><b>280 Vanderbilt Beach Road, Naples, FL</b><br/>
-	Overnight accommodation is available on a first come, first served basis at your own expense the night of December 13th.  A room block of 30 Coastal view rooms is being held at a rate of $179 + resort fee +  $12/night valet parking + tax.  The group rate will be offered for Saturday night <b>based on availability at time of request.  For individual reservations, call 1-888-856-4380 and reference Gartner, Inc. - Room Block for Holiday Party.</b> In order to ensure you are billed accurately, you MUST reference this group name and rate.  You will be asked to guarantee your reservation with a major credit card.  In order to avoid a penalty fee of the amount of your stay, reservations must be cancelled forty-eight (48) hours prior to the individual arrival date.  The room block expires November 22nd or when the rooms sell out, whichever occurs first. </p>
+	<p><b>Sanibel Harbour Marriott Resort & Spa</b><br/><b>17260 Harbour Point Dr, Ft Myers, FL 33908</b><br/>
+	Overnight accommodation is available on a first come, first served basis at your own expense the night of December 13th.  A room block of 60 Coastal view rooms is being held at a rate of $119 + tax.  The group rate will be offered for Saturday night <b>based on availability at time of request.  For individual reservations, call 1-800-767-7777 and reference Gartner, Inc. - Room Block for Holiday Party.</b> In order to ensure you are billed accurately, you MUST reference this group name and rate.  You will be asked to guarantee your reservation with a major credit card.  In order to avoid a penalty fee of the amount of your stay, reservations must be cancelled forty-eight (48) hours prior to the individual arrival date.  The room block expires December 6th or when the rooms sell out, whichever occurs first. </p>
 	<br />
 	<p style="color:#0065a4">Dress Code:</p>
 	<p>Cocktail Attire</p>
